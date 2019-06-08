79cfe75466101cf951d1e055576d6eebd6ceb51f
Marcos Rodrigo <marcosrlanuza@hotmail.com>
2019-06-08 19:48:37 +0200

    Configurar slider con imágenes o modo carrusel

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.galeria').bxSlider({
+        mode: 'fade',
+        captions: true,
+        slideWidth: 1200,
+        responsive: true,
+        pager: false,
+        auto: true,
+        speed: 1000
+    });
+});

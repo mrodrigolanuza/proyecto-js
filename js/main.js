3564c260319e8c94bcc49c504b304cb272541fe1
Marcos Rodrigo <marcosrlanuza@hotmail.com>
2019-06-09 10:27:48 +0200

    Insertar un acordeón de informacioń en la página about

--- js/main.js
+++ js/main.js
@@ -1,52 +1,56 @@
 $(document).ready(function(){
     //Slider, configurar plugin
-    $('.galeria').bxSlider({
-        mode: 'fade',
-        captions: true,
-        slideWidth: 1200,
-        responsive: true,
-        pager: false,
-        auto: true,
-        speed: 1000
-    });
+    if(window.location.href.indexOf('index')>-1){    //Solamente si estamos en la página index.html (existe la palabra index en la URL)
+        $('.galeria').bxSlider({
+            mode: 'fade',
+            captions: true,
+            slideWidth: 1200,
+            responsive: true,
+            pager: false,
+            auto: true,
+            speed: 1000
+        });
+    }
 
     //Posts o artículos de ejemplo (array de objetos JSON)
-    var posts = [
-        {
-            title: 'Prueba de título 1',
-            date:'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
-            content: 'Vestibulum sed libero sed nisi fermentum dapibus. In tortor erat, consequat in lorem ut, ullamcorper malesuada tortor. Aenean non augue aliquam turpis egestas maximus quis non lacus. Suspendisse facilisis, mi et tempor vestibulum, felis lacus molestie tellus, eu tempor mi ipsum a libero. Nullam id euismod urna, non euismod mi. Quisque at ex ac dolor commodo blandit. Ut porttitor, eros in dapibus egestas, ante est hendrerit ligula, tempor finibus nunc ante nec nisl. Nulla at ante sit amet ligula aliquet aliquet. Fusce et erat facilisis, fringilla tortor et, finibus odio. Nullam sagittis euismod justo, ac tempus diam mollis vel. Nam sem erat, sollicitudin id viverra porta, imperdiet tincidunt mi. Mauris efficitur nisl nulla, sit amet condimentum augue placerat laoreet.'
-        },
-        {
-            title: 'Prueba de título 2',
-            date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
-            content: 'Vestibulum sed libero sed nisi fermentum dapibus. In tortor erat, consequat in lorem ut, ullamcorper malesuada tortor. Aenean non augue aliquam turpis egestas maximus quis non lacus. Suspendisse facilisis, mi et tempor vestibulum, felis lacus molestie tellus, eu tempor mi ipsum a libero. Nullam id euismod urna, non euismod mi. Quisque at ex ac dolor commodo blandit. Ut porttitor, eros in dapibus egestas, ante est hendrerit ligula, tempor finibus nunc ante nec nisl. Nulla at ante sit amet ligula aliquet aliquet. Fusce et erat facilisis, fringilla tortor et, finibus odio. Nullam sagittis euismod justo, ac tempus diam mollis vel. Nam sem erat, sollicitudin id viverra porta, imperdiet tincidunt mi. Mauris efficitur nisl nulla, sit amet condimentum augue placerat laoreet.'
-        },
-        {
-            title: 'Prueba de título 3',
-            date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
-            content: 'Vestibulum sed libero sed nisi fermentum dapibus. In tortor erat, consequat in lorem ut, ullamcorper malesuada tortor. Aenean non augue aliquam turpis egestas maximus quis non lacus. Suspendisse facilisis, mi et tempor vestibulum, felis lacus molestie tellus, eu tempor mi ipsum a libero. Nullam id euismod urna, non euismod mi. Quisque at ex ac dolor commodo blandit. Ut porttitor, eros in dapibus egestas, ante est hendrerit ligula, tempor finibus nunc ante nec nisl. Nulla at ante sit amet ligula aliquet aliquet. Fusce et erat facilisis, fringilla tortor et, finibus odio. Nullam sagittis euismod justo, ac tempus diam mollis vel. Nam sem erat, sollicitudin id viverra porta, imperdiet tincidunt mi. Mauris efficitur nisl nulla, sit amet condimentum augue placerat laoreet.'
-        },
-        {
-            title: 'Prueba de título 4',
-            date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
-            content: 'Vestibulum sed libero sed nisi fermentum dapibus. In tortor erat, consequat in lorem ut, ullamcorper malesuada tortor. Aenean non augue aliquam turpis egestas maximus quis non lacus. Suspendisse facilisis, mi et tempor vestibulum, felis lacus molestie tellus, eu tempor mi ipsum a libero. Nullam id euismod urna, non euismod mi. Quisque at ex ac dolor commodo blandit. Ut porttitor, eros in dapibus egestas, ante est hendrerit ligula, tempor finibus nunc ante nec nisl. Nulla at ante sit amet ligula aliquet aliquet. Fusce et erat facilisis, fringilla tortor et, finibus odio. Nullam sagittis euismod justo, ac tempus diam mollis vel. Nam sem erat, sollicitudin id viverra porta, imperdiet tincidunt mi. Mauris efficitur nisl nulla, sit amet condimentum augue placerat laoreet.'
-        }
-    ]
+    if(window.location.href.indexOf('index')>-1){    //Solamente si estamos en la página index.html (existe la palabra index en la URL)
+        var posts = [
+            {
+                title: 'Prueba de título 1',
+                date:'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+                content: 'Vestibulum sed libero sed nisi fermentum dapibus. In tortor erat, consequat in lorem ut, ullamcorper malesuada tortor. Aenean non augue aliquam turpis egestas maximus quis non lacus. Suspendisse facilisis, mi et tempor vestibulum, felis lacus molestie tellus, eu tempor mi ipsum a libero. Nullam id euismod urna, non euismod mi. Quisque at ex ac dolor commodo blandit. Ut porttitor, eros in dapibus egestas, ante est hendrerit ligula, tempor finibus nunc ante nec nisl. Nulla at ante sit amet ligula aliquet aliquet. Fusce et erat facilisis, fringilla tortor et, finibus odio. Nullam sagittis euismod justo, ac tempus diam mollis vel. Nam sem erat, sollicitudin id viverra porta, imperdiet tincidunt mi. Mauris efficitur nisl nulla, sit amet condimentum augue placerat laoreet.'
+            },
+            {
+                title: 'Prueba de título 2',
+                date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+                content: 'Vestibulum sed libero sed nisi fermentum dapibus. In tortor erat, consequat in lorem ut, ullamcorper malesuada tortor. Aenean non augue aliquam turpis egestas maximus quis non lacus. Suspendisse facilisis, mi et tempor vestibulum, felis lacus molestie tellus, eu tempor mi ipsum a libero. Nullam id euismod urna, non euismod mi. Quisque at ex ac dolor commodo blandit. Ut porttitor, eros in dapibus egestas, ante est hendrerit ligula, tempor finibus nunc ante nec nisl. Nulla at ante sit amet ligula aliquet aliquet. Fusce et erat facilisis, fringilla tortor et, finibus odio. Nullam sagittis euismod justo, ac tempus diam mollis vel. Nam sem erat, sollicitudin id viverra porta, imperdiet tincidunt mi. Mauris efficitur nisl nulla, sit amet condimentum augue placerat laoreet.'
+            },
+            {
+                title: 'Prueba de título 3',
+                date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+                content: 'Vestibulum sed libero sed nisi fermentum dapibus. In tortor erat, consequat in lorem ut, ullamcorper malesuada tortor. Aenean non augue aliquam turpis egestas maximus quis non lacus. Suspendisse facilisis, mi et tempor vestibulum, felis lacus molestie tellus, eu tempor mi ipsum a libero. Nullam id euismod urna, non euismod mi. Quisque at ex ac dolor commodo blandit. Ut porttitor, eros in dapibus egestas, ante est hendrerit ligula, tempor finibus nunc ante nec nisl. Nulla at ante sit amet ligula aliquet aliquet. Fusce et erat facilisis, fringilla tortor et, finibus odio. Nullam sagittis euismod justo, ac tempus diam mollis vel. Nam sem erat, sollicitudin id viverra porta, imperdiet tincidunt mi. Mauris efficitur nisl nulla, sit amet condimentum augue placerat laoreet.'
+            },
+            {
+                title: 'Prueba de título 4',
+                date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+                content: 'Vestibulum sed libero sed nisi fermentum dapibus. In tortor erat, consequat in lorem ut, ullamcorper malesuada tortor. Aenean non augue aliquam turpis egestas maximus quis non lacus. Suspendisse facilisis, mi et tempor vestibulum, felis lacus molestie tellus, eu tempor mi ipsum a libero. Nullam id euismod urna, non euismod mi. Quisque at ex ac dolor commodo blandit. Ut porttitor, eros in dapibus egestas, ante est hendrerit ligula, tempor finibus nunc ante nec nisl. Nulla at ante sit amet ligula aliquet aliquet. Fusce et erat facilisis, fringilla tortor et, finibus odio. Nullam sagittis euismod justo, ac tempus diam mollis vel. Nam sem erat, sollicitudin id viverra porta, imperdiet tincidunt mi. Mauris efficitur nisl nulla, sit amet condimentum augue placerat laoreet.'
+            }
+        ]
     
-    //Insertar la info de los posts de ejemplo en el HTML de la página, mediante una plantilla HTML y JavaScript 
-    posts.forEach((item, index) => {
-        var post = `
-            <article class="post">
-                <h2>${item.title}</h2>
-                <span class="date">${item.date}</span>
-                <p>${item.content}</p>
-                <a href="#" class="button-more">Leer más</a>
-            </article>    
-        `;
-        $("#posts").append(post);
-    });
-
+        //Insertar la info de los posts de ejemplo en el HTML de la página, mediante una plantilla HTML y JavaScript 
+        posts.forEach((item, index) => {
+            var post = `
+                <article class="post">
+                    <h2>${item.title}</h2>
+                    <span class="date">${item.date}</span>
+                    <p>${item.content}</p>
+                    <a href="#" class="button-more">Leer más</a>
+                </article>    
+            `;
+            $("#posts").append(post);
+        });
+    }
+    
     //Botones de selección de tema
     var theme = $("#tema-seleccionado");            //Variable que almacena la referencia al fichero de estilos en index.html
     $("#to-green").click(function(){
@@ -88,6 +92,9 @@ $(document).ready(function(){
         });
     }
     
-    
+    //Acordeón de la página about
+    if(window.location.href.indexOf('about')>-1){    //Solamente si estamos en la página about.html (existe la palabra about en la URL)
+        $("#acordeon").accordion();
+    }
 
 });
